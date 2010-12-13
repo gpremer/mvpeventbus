6408bad3aad429370e7e01756432c935602cdb52
Geert Premereur <geert.premereur@gmail.com>
2010-12-13 17:20:00 +0100

    Added menu bar without actions

--- mvp-vaadin-example/src/main/java/net/premereur/mvp/example/vaadin/app/ApplicationPresenter.java
+++ mvp-vaadin-example/src/main/java/net/premereur/mvp/example/vaadin/app/ApplicationPresenter.java
@@ -3,30 +3,61 @@ package net.premereur.mvp.example.vaadin.app;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.vaadin.Application;
+import com.vaadin.ui.MenuBar;
 import com.vaadin.ui.Window;
+import com.vaadin.ui.MenuBar.Command;
+import com.vaadin.ui.MenuBar.MenuItem;
 import com.vaadin.ui.Window.CloseEvent;
 
 import net.premereur.mvp.core.EventBus;
 import net.premereur.mvp.core.guice.BasePresenter;
 
 @Singleton
-public final class ApplicationPresenter extends BasePresenter<ApplicationWindow, ApplicationBus> {
+public final class ApplicationPresenter extends
+		BasePresenter<ApplicationWindow, ApplicationBus> {
 
 	@Inject
-	public ApplicationPresenter(final EventBus eventBus, final ApplicationWindow view) {
+	public ApplicationPresenter(final EventBus eventBus,
+			final ApplicationWindow view) {
 		super((ApplicationBus) eventBus, view);
 	}
 
-
-	@SuppressWarnings("serial")
 	public void onInit(final Application application) {
 		ApplicationWindow main = getView();
 		application.setMainWindow(main);
+		addAppListener(application, main);
+		addMenu(main);
+	}
+
+	@SuppressWarnings("serial")
+	private void addAppListener(final Application application,
+			ApplicationWindow main) {
 		main.addListener(new Window.CloseListener() {
-			
+
 			@Override
 			public void windowClose(CloseEvent e) {
 				getEventBus().close(application);
+			}
+		});
+	}
+
+	@SuppressWarnings("serial")
+	private void addMenu(final ApplicationWindow main) {
+		final MenuBar menuBar = new MenuBar();
+		main.addComponent(menuBar);
+		final MenuItem appItem = menuBar.addItem("Application", null);
+		appItem.addItem("Categories", new Command() {
+			
+			@Override
+			public void menuSelected(MenuItem selectedItem) {
+				// TODO send to event bus				
+			}
+		});
+		appItem.addItem("Products", new Command() {
+			
+			@Override
+			public void menuSelected(MenuItem selectedItem) {
+				// TODO send to event bus				
 			}
 		});
 	}
--- mvp-vaadin-example/src/main/java/net/premereur/mvp/example/vaadin/app/ApplicationWindow.java
+++ mvp-vaadin-example/src/main/java/net/premereur/mvp/example/vaadin/app/ApplicationWindow.java
@@ -1,16 +1,13 @@
 package net.premereur.mvp.example.vaadin.app;
 
-import com.vaadin.ui.Label;
-import com.vaadin.ui.Window;
-
 import net.premereur.mvp.core.View;
+
+import com.vaadin.ui.Window;
 
 @SuppressWarnings("serial")
 public class ApplicationWindow extends Window implements View {
 
 	public ApplicationWindow() {
 		super("Sample Vaadin MVP Application");
-		final Label label = new Label("Hello Vaadin user");
-		addComponent(label);
 	}
 }
