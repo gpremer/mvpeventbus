fdaa57e4de03ceed4e1da8d6e596cb5ca67d7e70
Geert Premereur <geert.premereur@gmail.com>
2010-12-18 17:01:18 +0100

    Fixes for checkstyle

--- mvp-vaadin-example/src/main/java/net/premereur/mvp/example/vaadin/SampleApplication.java
+++ mvp-vaadin-example/src/main/java/net/premereur/mvp/example/vaadin/SampleApplication.java
@@ -9,17 +9,23 @@ import net.premereur.mvp.example.vaadin.categorymgt.CategoryMgtModule;
 
 import com.vaadin.Application;
 
+/**
+ * An application for demonstrating how mvpeventbus could be used with Vaadin.
+ * 
+ * @author gpremer
+ * 
+ */
 @SuppressWarnings("serial")
 public class SampleApplication extends Application {
 
-	private static final EventBusFactory EVENT_BUS_FACTORY;
+    private static final EventBusFactory EVENT_BUS_FACTORY;
 
-	static {
-		EVENT_BUS_FACTORY = new GuiceEventBusFactory(new ApplicationModule(), new CategoryMgtModule());
-	}
+    static {
+        EVENT_BUS_FACTORY = new GuiceEventBusFactory(new ApplicationModule(), new CategoryMgtModule());
+    }
 
-	@Override
-	public final void init() {
-		EVENT_BUS_FACTORY.createEventBus(ApplicationBus.class, CategoryMgtBus.class).init(this);		
-	}
+    @Override
+    public final void init() {
+        EVENT_BUS_FACTORY.createEventBus(ApplicationBus.class, CategoryMgtBus.class).init(this);
+    }
 }
--- mvp-vaadin-example/src/main/java/net/premereur/mvp/example/vaadin/app/ApplicationBus.java
+++ mvp-vaadin-example/src/main/java/net/premereur/mvp/example/vaadin/app/ApplicationBus.java
@@ -5,15 +5,20 @@ import net.premereur.mvp.core.EventBus;
 
 import com.vaadin.Application;
 
+/**
+ * Event bus segment that routes major, application-level events.
+ * 
+ * @author gpremer
+ */
 public interface ApplicationBus extends EventBus {
 
-	@Event(ApplicationPresenter.class)
-	void init(Application app);
+    @Event(ApplicationPresenter.class)
+    void init(Application app);
 
-	@Event(ApplicationPresenter.class)
-	void close(Application app);
+    @Event(ApplicationPresenter.class)
+    void close(Application app);
 
-	@Event(ApplicationPresenter.class)
-	void selectCategoryMgt();
-	
+    @Event(ApplicationPresenter.class)
+    void selectCategoryMgt();
+
 }
